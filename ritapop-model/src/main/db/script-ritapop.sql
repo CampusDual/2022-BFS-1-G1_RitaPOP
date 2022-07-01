f64fc313d70954064a6d4adc5c35102f5177f32b
Tomás Carralero <37511894+tomascarralero@users.noreply.github.com>
2022-07-01 11:48:06 +0200

    Modify .gitignore to add HSQL workaround and Adds script.sql file to manage teamwork with DDBB HSQL

---
+++ ritapop-model/src/main/db/script-ritapop.sql
@@ -0,0 +1,5 @@
+-- Aquí iremos pegando las modificaciones que hagamos en la base de datos creando dos comentarios para acotar los cambios realizados para una nueva historia
+
+-- INICIO Cambios para FG4-XX FECHA: dd-mm-YYYY hh:mm
+
+-- FIN cambios para FG4-XX FECHA: dd-mm-YYYY hh:mm
