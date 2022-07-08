a91297c00278f942afe1ea2f0b399f09f422409f
cardizaDEV <cardizaDEV@gmail.com>
2022-07-08 14:31:50 +0200

    Finished permissions structure

--- ritapop-model/src/main/java/com/grupo4/ritapop/model/core/service/PermissionsService.java
+++ ritapop-model/src/main/java/com/grupo4/ritapop/model/core/service/PermissionsService.java
@@ -6,13 +6,16 @@ import com.grupo4.ritapop.api.core.service.PermissionsApi;
 import com.grupo4.ritapop.model.core.dao.PermissionsDao;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
+import com.ontimize.jee.common.services.user.UserInformation;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service("PermissionsService")
@@ -31,9 +34,19 @@ public class PermissionsService implements PermissionsApi {
     List<String> attr = new ArrayList<String>();
     attr.add(ATTR_PERMISSIONS);
     Map<String, Object> map = new HashMap<>();
-    map.put("ID_ROLENAME", "0");
+    String loggedUserRolname = getLoggedUserRolHighest();
+    map.put("ROLENAME", loggedUserRolname);
     EntityResult query = this.daoHelper.query(this.permissionsDao, map, attr);
     entityResultMap.addRecord(new HashMap<>(){{put("permission",((List) query.get(ATTR_PERMISSIONS)).get(0));}});
     return entityResultMap;
+  }
+
+  public String getLoggedUserRolHighest(){
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    UserInformation p = (UserInformation) authentication.getPrincipal();
+    Collection<GrantedAuthority> aut = p.getAuthorities();
+    Optional<GrantedAuthority> roles = aut.stream().findFirst();
+    String rol = roles.get().getAuthority();
+    return rol;
   }
 }
--- ritapop-ws/src/main/java/com/grupo4/ritapop/ws/core/rest/PermissionController.java
+++ ritapop-ws/src/main/java/com/grupo4/ritapop/ws/core/rest/PermissionController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("")
 public class PermissionController extends ORestController<PermissionsApi> {
