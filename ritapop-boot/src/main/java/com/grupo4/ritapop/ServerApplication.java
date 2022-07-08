2ae1da2aa20e547eaf0e6ddbe25043d9fa74fd34
DESKTOP-93NJ21I\fandi <javier.fandino@campusdual.com>
2022-07-08 15:32:07 +0200

    created scheduled service

--- ritapop-boot/src/main/java/com/grupo4/ritapop/ServerApplication.java
+++ ritapop-boot/src/main/java/com/grupo4/ritapop/ServerApplication.java
@@ -1,13 +1,21 @@
 package com.grupo4.ritapop;
-
+import com.grupo4.ritapop.model.core.service.VipClientService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+		CallVipClientService();
 	}
-
+	@Scheduled(fixedRate = 3000)
+	private static void CallVipClientService(){
+		VipClientService vipClientService = new VipClientService();
+		vipClientService.callVipClientService();
+	}
 }
---
+++ ritapop-model/src/main/java/com/grupo4/ritapop/model/core/service/VipClientService.java
@@ -0,0 +1,7 @@
+package com.grupo4.ritapop.model.core.service;
+
+public class VipClientService {
+    public void callVipClientService(){
+        System.out.println("hola");
+    }
+}
