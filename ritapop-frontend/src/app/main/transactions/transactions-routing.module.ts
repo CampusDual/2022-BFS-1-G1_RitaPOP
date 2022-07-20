e344d81ccf79b1919f85a8d571a49cafe48682ca
DESKTOP-93NJ21I\fandi <javier.fandino@campusdual.com>
2022-07-20 13:27:59 +0200

    added insert transaction form

--- ritapop-frontend/src/app/main/transactions/transactions-routing.module.ts
+++ ritapop-frontend/src/app/main/transactions/transactions-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TransactionsHomeComponent } from './transactions-home/transactions-home.component';
+import { TransactionsNewComponent } from './transactions-new/transactions-new.component';
 
 
 const routes: Routes = [{
   path: '',
   component: TransactionsHomeComponent
-}];
+},
+{
+  path:"new",
+  component: TransactionsNewComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
