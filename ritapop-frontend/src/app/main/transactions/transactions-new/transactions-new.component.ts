3948544a5e18268584a63e46917664a608c22404
tomascarralero <tcarralero@gmail.com>
2022-08-02 20:39:11 +0200

    Added min/max validation to currency at new and detail forms of Transactions view

--- ritapop-frontend/src/app/main/transactions/transactions-new/transactions-new.component.ts
+++ ritapop-frontend/src/app/main/transactions/transactions-new/transactions-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidatorFn, ValidationErrors, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-transactions-new',
@@ -7,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionsNewComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  maxValidator(control: FormControl): ValidationErrors  {
+    let result = {};
+    if (control.value && control.value > 999999.99) {
+      result['requiredMax'] = true;
+    }
+    return result;
+  }
+
+  minValidator(control: FormControl): ValidationErrors  {
+    let result = {};
+    if (control.value && control.value < 0) {
+      result['requiredMin'] = true;
+    }
+    return result;
+  }
 
 }
