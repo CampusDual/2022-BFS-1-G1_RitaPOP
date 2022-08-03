270a10831c68f0a326e3bf0833537bd9e8ca2882
cardizaDEV <cardizaDEV@gmail.com>
2022-08-03 15:11:15 +0200

    Migrated from side menu to top menu and added logo to menu

--- ritapop-frontend/src/app/main/main.component.ts
+++ ritapop-frontend/src/app/main/main.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
+
 export class MainComponent implements OnInit{
+
+  username : string;
+
   constructor(){
   }
   ngOnInit(){
+    this.username = "cincuenta";
   }
 }
 
-
+export class MainModule {}
 
