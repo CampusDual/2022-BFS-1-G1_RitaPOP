afae099c811dd328b76bbfdf7a2dd1545d569aa4
cardizaDEV <cardizaDEV@gmail.com>
2022-08-03 15:39:12 +0200

    Changed menu icons

--- ritapop-frontend/src/app/shared/app.menu.config.ts
+++ ritapop-frontend/src/app/shared/app.menu.config.ts
@@ -1,8 +1,8 @@
 import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
-  { id: 'home', name: 'HOME', icon: 'dashboard', route: '/main/home' },
-  { id: 'users', name: 'USERS', icon: 'people', route: '/main/users'},
+  { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
+  { id: 'users', name: 'USERS', icon: 'person', route: '/main/users'},
   { id: 'client', name: 'CLIENT', icon: 'people', route: '/main/clients'},
-  { id: 'transaction', name: 'TRANSACTIONS', icon: 'business_center', route: '/main/transactions'},
+  { id: 'transaction', name: 'TRANSACTIONS', icon: 'credit_card', route: '/main/transactions'},
 ];
