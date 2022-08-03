63e768e5bf32b5bc5aa8891fbceae06e4532dbb5
tomascarralero <tcarralero@gmail.com>
2022-08-03 10:59:59 +0200

    Controlled phone length changing BBDD type to integer and using custom validators at Clients and Users

--- ritapop-frontend/src/app/main/transactions/transactions-new/transactions-new.component.ts
+++ ritapop-frontend/src/app/main/transactions/transactions-new/transactions-new.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ValidatorFn, ValidationErrors, FormControl } from '@angular/forms';
+import { ValidationErrors, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-transactions-new',
