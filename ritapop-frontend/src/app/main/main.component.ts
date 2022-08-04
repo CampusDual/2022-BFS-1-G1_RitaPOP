4204fe45bb4db1443c98fbaf1c8e58af2cc9c107
cardizaDEV <cardizaDEV@gmail.com>
2022-08-04 12:06:00 +0200

    Hotfix

--- ritapop-frontend/src/app/main/main.component.ts
+++ ritapop-frontend/src/app/main/main.component.ts
@@ -8,12 +8,9 @@ import { Component, OnInit } from '@angular/core';
 
 export class MainComponent implements OnInit{
 
-  username : string;
-
   constructor(){
   }
   ngOnInit(){
-    this.username = "cincuenta";
   }
 }
 
