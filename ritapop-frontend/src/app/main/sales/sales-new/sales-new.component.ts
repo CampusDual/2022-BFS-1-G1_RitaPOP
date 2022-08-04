bec819a494e9348389e44bdc8d9a9c8756802233
tomascarralero <tcarralero@gmail.com>
2022-08-04 20:16:06 +0200

    Sales new component WIP

---
+++ ritapop-frontend/src/app/main/sales/sales-new/sales-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-new',
+  templateUrl: './sales-new.component.html',
+  styleUrls: ['./sales-new.component.css']
+})
+export class SalesNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ritapop-frontend/src/app/main/sales/sales.module.ts
+++ ritapop-frontend/src/app/main/sales/sales.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { SalesRoutingModule } from './sales-routing.module';
 import { SalesHomeComponent } from './sales-home/sales-home.component';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { SalesNewComponent } from './sales-new/sales-new.component';
 
 @NgModule({
-  declarations: [SalesHomeComponent],
+  declarations: [SalesHomeComponent, SalesNewComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
