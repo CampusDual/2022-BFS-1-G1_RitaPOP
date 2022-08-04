0835a2b8147e5e8f0a4ef1b247ecccfc9ab68962
DESKTOP-93NJ21I\fandi <javier.fandino@campusdual.com>
2022-08-04 18:44:59 +0200

    Added material icons

--- ritapop-frontend/src/app/shared/app.menu.config.ts
+++ ritapop-frontend/src/app/shared/app.menu.config.ts
@@ -2,9 +2,9 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'users', name: 'USERS', icon: 'people', route: '/main/users'},
+  { id: 'users', name: 'USERS', icon: 'account_circle', route: '/main/users'},
   { id: 'client', name: 'CLIENT', icon: 'people', route: '/main/clients'},
   { id: 'transaction', name: 'TRANSACTIONS', icon: 'business_center', route: '/main/transactions'},
-  { id: 'sales', name: 'SALES', icon: 'business_center', route: '/main/sales'},
+  { id: 'sales', name: 'SALES', icon: 'store', route: '/main/sales'},
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
