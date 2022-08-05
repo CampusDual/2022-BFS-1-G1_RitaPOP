bce61e6ab9d60a197c2d3b92e7fb67619e8d48a1
DonatoRM <donatoramos@hotmail.com>
2022-08-05 14:04:02 +0200

    WIP SalesNewComponent

--- ritapop-frontend/src/app/main/sales/sales-new/sales-new.component.ts
+++ ritapop-frontend/src/app/main/sales/sales-new/sales-new.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { OComboComponent, OTextInputComponent } from 'ontimize-web-ngx';
+import { OComboComponent, OEmailInputComponent, OIntegerInputComponent, OTextInputComponent } from 'ontimize-web-ngx';
+import { ValidationErrors, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sales-new',
@@ -13,10 +14,35 @@ export class SalesNewComponent implements OnInit {
   ngOnInit() {
   }
 
-  addNameAndSurnameSeller($event: any, combo: OComboComponent, name: OTextInputComponent, surname: OTextInputComponent){
+  addDataSeller($event: any, combo: OComboComponent, name: OTextInputComponent, surname: OTextInputComponent,email: OEmailInputComponent, phone_number:OIntegerInputComponent){
 
-    alert('Hola');
-    //name.setValue=combo.get
+    let nameCombo:String=combo.getSelectedRecord()['NAME'];
+    let surnameCombo:String=combo.getSelectedRecord()['SURNAME'];
+    let emailCombo:String=combo.getSelectedRecord()['EMAIL'];
+    let phoneNumberCombo:String=combo.getSelectedRecord()['PHONE_NUMBER'];
+    name.setValue('');
+    surname.setValue('');
+    email.setValue('');
+    phone_number.setValue('');
+    name.setValue(nameCombo);
+    surname.setValue(surnameCombo);
+    email.setValue(emailCombo);
+    phone_number.setValue(phoneNumberCombo);
   }
 
+  maxValidator(control: FormControl): ValidationErrors  {
+    let result = {};
+    if (control.value && control.value > 999999.99) {
+      result['requiredMax'] = true;
+    }
+    return result;
+  }
+
+  minValidator(control: FormControl): ValidationErrors  {
+    let result = {};
+    if (control.value && control.value < 0) {
+      result['requiredMin'] = true;
+    }
+    return result;
+  }
 }
