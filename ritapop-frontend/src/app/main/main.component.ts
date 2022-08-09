eeaba3aacd5afbbefc2238bf58ff64ce859a25c5
cinta <cintatafur@gmail.com>
2022-08-09 14:53:35 +0200

    Views are now responsive, added onclick event to menu items

--- ritapop-frontend/src/app/main/main.component.ts
+++ ritapop-frontend/src/app/main/main.component.ts
@@ -1,20 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"],
 })
-
 export class MainComponent implements OnInit {
 
   constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.setActiveItem();
+      }
+    });
   }
-  ngOnInit() {
+  
+  ngOnInit() {}
+  setActiveItem() {
+    const url = this.router.url;
+    document.querySelectorAll("o-bar-menu-item").forEach((el) => {
+      if (el.getAttribute("ng-reflect-route") == url) {
+        el.querySelector("a.mat-list-item").classList.add("optionClicked");
+      }
+      else {
+        el.querySelector("a.mat-list-item").classList.remove("optionClicked");
+      }
+    });
   }
 
+  ngAfterViewInit() {
+    const url = this.router.url;
+    document.querySelectorAll('o-bar-menu-item').forEach(el => {
+      if (el.getAttribute('ng-reflect-route') == url) {
+        el.querySelector('a.mat-list-item').classList.add("optionClicked");
+      }
+    })
+  }
 }
 
-export class MainModule { }
-
+export class MainModule {}
